016c334bfa042b784824bb36d0a948a8c4003649
Isai Lopez <ilopezro@ucsc.edu>
2019-06-25 01:09:29 -0700

    added navbar components

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,48 @@
 import React, {Component} from 'react';
 
 class Navbar extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      pages: [
+        {
+          name: "Home",
+          path: "/"
+        },
+        {
+          name: "About Me",
+          path: "/aboutMe"
+        },
+        {
+          name: "Class Links",
+          path: "/classLinks"
+        },
+        {
+          name: "Contact",
+          path: "/contact"
+        }
+      ]
+    }
+  }
+
+  getNavComponents(){
+    return this.state.pages.map((item, i) => {
+      return (
+        <li className="nav-item">
+          <a className="nav-link" href={item.path} key={i}>{item.name}</a>
+        </li>
+      )
+      }
+    )
+  }
+
   render(){
     return(
-      <div>
-      Hello from navbar
-      </div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+						<ul className="navbar-nav ml-auto">{this.getNavComponents()}</ul>
+					</div>
+      </nav>
     )
   }
 }
