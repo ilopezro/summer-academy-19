9adc54eb3a00b68479f6594bdf3e85dcbba14c07
Isai Lopez <ilopezro@ucsc.edu>
2019-06-25 01:53:24 -0700

    moved routes into src folder

--- routes/routes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {BroswerRouter, Route, Switch} from 'react-router-dom'
-import App from '../src/components/App'
-
-class Routes {
-
-}
-
-export default Routes
---
+++ src/routes/routes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import App from '../components/App'
+import ClassLinks from "../components/ClassLinks"
+import Contact from "../components/Contact"
+
+export default () => {
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={App} />
+                <Route path="/classLinks" exact component={ClassLinks} />
+                <Route path="/contact" exact component={Contact} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
