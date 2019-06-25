072cad3256c0ab217e53e51014b0645c5e7a439a
Isai Lopez <ilopezro@ucsc.edu>
2019-06-25 01:53:47 -0700

    added classlinks page and contact page

---
+++ src/components/ClassLinks.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import Navbar from './navbar'
+
+class ClassLinks extends Component{
+    render(){
+        return(
+            <div className="header">
+                <Navbar />
+               Hello from ClassLinks
+            </div>
+        )
+    }
+}
+
+export default ClassLinks
---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import Navbar from './navbar'
+
+class Contact extends Component{
+    render(){
+        return(
+            <div>
+                <Navbar/>
+                Hello from contact
+            </div>
+        )
+    }
+}
+
+export default Contact
