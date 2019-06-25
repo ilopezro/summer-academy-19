448c97a3e92eb666ed8ce68846c40f4474f00a0a
Isai Lopez <ilopezro@ucsc.edu>
2019-06-25 01:55:20 -0700

    added routes to all pages

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import Navbar from './navbar'
+
+class AboutMe extends Component{
+    render(){
+        return(
+            <div>
+                <Navbar/>
+                Hello from AboutMe
+            </div>
+        )
+    }
+}
+
+export default AboutMe
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import App from '../components/App'
+import AboutMe from "../components/AboutMe"
 import ClassLinks from "../components/ClassLinks"
 import Contact from "../components/Contact"
 
@@ -9,6 +10,7 @@ export default () => {
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={App} />
+                <Route path="/aboutMe" exact component={AboutMe} />
                 <Route path="/classLinks" exact component={ClassLinks} />
                 <Route path="/contact" exact component={Contact} />
             </Switch>
