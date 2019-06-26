7c5920e790c4ac1344224462eceb5d205ede94e7
Isai Lopez <ilopezro@ucsc.edu>
2019-06-26 10:53:01 -0700

    imported statements added

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,5 +1,10 @@
 import React, {Component} from 'react'
 import Navbar from './navbar'
+import '../styles/aboutme.css'
+import headshot from '../assets/headshot.jpg'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class AboutMe extends Component{
     render(){
