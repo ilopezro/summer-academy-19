61acb7523bbe1008122cacad5e44ef45c2ae7361
Isai Lopez <ilopezro@ucsc.edu>
2019-06-26 00:47:01 -0700

    added table to class links

--- src/components/ClassLinks.jsx
+++ src/components/ClassLinks.jsx
@@ -1,12 +1,77 @@
 import React, {Component} from 'react'
 import Navbar from './navbar'
+import '../styles/classlinks.css'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class ClassLinks extends Component{
+    constructor(){
+        super()
+        this.state = {
+            links: [
+                {
+                    id: 1,
+                    name: "Student", 
+                    description: "N/A",
+                    link: "/"
+                },
+                {
+                    id: 2,
+                    name: "Student", 
+                    description: "N/A",
+                    link: "/"
+                },
+                {
+                id: 3,
+                name: "Student", 
+                description: "N/A",
+                link: "/"
+                }
+            ]
+        }
+    }
+
+    getTableRows(){
+        return this.state.links.map((item, i) => {
+            return (
+                <tr key={i}>
+                    <td>{item.name} {item.id}</td>
+                    <td>{item.description}</td>
+                    <td>{item.link}</td>
+                </tr>
+            );
+        })
+    }
+
     render(){
         return(
             <div className="header">
                 <Navbar />
-               Hello from ClassLinks
+                <Container fluid="true">
+                    <Row>
+                        <Col>
+                            <p className="title">Class Links</p>
+                            <p className="titleDes">Here, we will have all the links to your sites!!</p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th> Student Name</th>
+                                        <th> Site Description</th>
+                                        <th> Link </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {this.getTableRows()}
+                                </tbody>
+                            </table>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
     }
