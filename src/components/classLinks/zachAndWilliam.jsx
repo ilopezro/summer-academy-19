f3ad810584f1d8e655f21b9319b14384dc48a2f0
Isai Lopez <ilopezro@ucsc.edu>
2019-07-09 15:13:14 -0700

    craeted test file

---
+++ src/components/classLinks/zachAndWilliam.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Navbar from '../navbar';
+
+class ZachAndWilliam extends Component{
+
+    render(){
+        return (
+            <div>
+                <Navbar />
+                Hello from Zach and William 
+            </div>
+        )
+    }
+}
+
+export default ZachAndWilliam; 
